9d84f50398d04220d6c71f80e51e5d3c541baad3
Siddharth Tanwar <83527491+sid-tanwar@users.noreply.github.com>
2022-01-05 16:12:31 +0530

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static String removeChars(String s, char[] arr){
+
+        StringBuilder st = new StringBuilder(s);
+        for(int i =0; i<arr.length; i++){
+
+            for(int j=0; j<st.length(); j++){
+
+                if(arr[i]==st.charAt(j)) {
+
+                    st = st.deleteCharAt(j);
+                }
+            }
+        }
+        String ab = new String(st);
+        return ab;
+    }
+
+    public static void main(String[] args) {
+        String str = "Siddharth Tanwar";
+        char[] a = {'d', 'a'};
+        System.out.println(removeChars(str, a));
+
+    }
+}
